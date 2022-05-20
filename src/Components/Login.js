fafe860ce07f6db12a5da29df49055afdf3155e9
ReactPro <vetri5cse@gmail.com>
2022-05-20 11:40:44 +0530

    new update

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,26 +1,41 @@
-import * as React from "react";
+
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
-import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
+import React,{Component} from 'react'
 
-export default function Login() {
+class Login extends Component{
+    constructor(){
+        super();
+        this.state={
+            email:'',
+            password:''
+        }
+        this.handleChange= this.handleChange.bind(this);
+        this.validateForm = this.validateForm.bind(this);
+    }
+    handleChange(e){
+        this.setState({[e.target.name]:e.target.value});
+    }
+    validateForm(e){
+        if(this.state.email==='' || this.state.password ===''){
+            alert("Dont leave anything blank");
+            e.preventDefault();
+        }
+    }
+    render(){
   return (
     <Box
-      component="form"
-      sx={{
-        "& > :not(style)": { m: 1, width: "25ch" },
-      }}
-      noValidate
-      autoComplete="off"
-    >
-      <TextField id="filled-basic" label="Email" variant="filled" />
+      component="form" sx={{"& > :not(style)": { m: 1, width: "25ch" },}}noValidate autoComplete="off">
+      <TextField id="emailInput" label="Email"  name="email" variant="filled"  value={this.state.email} onChange={this.handleChange}/>
       <br />
-      <TextField id="standard-basic" label="Password" variant="filled" />
+      <TextField id="passwordInput" label="Password"  name="password" variant="filled" value={this.state.password} onChange={this.handleChange} />
       <br />
-      <Stack spacing={2} direction="row">
-        <Button variant="contained">Login</Button>
-      </Stack>
+      
+        <Button variant="contained" onClick={this.validateForm}>Login</Button>
+     
     </Box>
   );
 }
+}
+export default Login;
